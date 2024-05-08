699434ce988f1d0091227b6c4547cfb9f27adcfd
Ahmed S. Mahil <106696203+Almahil249@users.noreply.github.com>
2024-05-08 10:57:41 +0000

    DATABASES set to django default sqlite db

--- studentsaffairs/settings.py
+++ studentsaffairs/settings.py
@@ -75,21 +75,12 @@ WSGI_APPLICATION = 'studentsaffairs.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': 'LPsZnJw7g0If',
-        'HOST': 'ep-wispy-cell-89913455-pooler.us-east-1.postgres.vercel-storage.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-   
 }
-DATABASES["default"]= dj_database_url.parse("postgres://admin:abHoUXQmMNsV2tQ3gtwlZ7mg2wUyd6aa@dpg-copkimtjm4es73a5j840-a.frankfurt-postgres.render.com/studentsdb_z8g0")
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
